fba621f0ce70295b218aadc52805af7f19ac133d
SESCNCFUARTYOM <perepelukovartem@gmail.com>
2021-12-07 05:18:22 +0300

    Добавлены

--- 4.py
+++ 4.py
@@ -1,16 +1,17 @@
-# !/usr/bin/env python3
-# -*- encoding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- encoding: utf-8
+
 import math
 
 if __name__ == "__main__":
-    n = int(input("n = "))
+    n = int(input())
     if n == 1:
         print(0)
     else:
         count = 0
         for i in range(2, int(math.sqrt(n) + 1)):
             if n % i == 0:
-                count = count + 1
+                count += 1
         if count == 0:
             print(1)
         else:
--- 5.py
+++ 5.py
@@ -2,5 +2,5 @@
 # -*- encoding: utf-8
 if __name__ == "__main__":
     for i in range(0, 17):
-        print("Гусей:", 2 * i)
         print("Кроликов:", 16 - i)
+        print("Гусей:", 2 * i)
